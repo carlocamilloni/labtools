e66a0f1ca0497b37726445bab48ca51b58a9c88a
carlocamilloni <carlo.camilloni@gmail.com>
2019-07-11 14:41:54 +0200

    improved block averaging to better account for concatenated replicas

--- plumed-stuff/run_block-aver.sh
+++ plumed-stuff/run_block-aver.sh
@@ -1,12 +1,40 @@
-#takes in input a file with n columns for N CV and the last column is a weight
-echo "processing file: " $1
-bl=`wc -l $1 | awk '{printf("%i\n", $1/4)}'`
-st=`wc -l $1 | awk '{printf("%i\n", $1/2/100)}'`
-max=`sort -k1 -n $1 | tail -1 | awk '{print $1}'`
-min=`sort -k1 -n -r $1 | tail -1 | awk '{print $1}'`
-kt=$2
-echo "max block size " $bl
-echo "block inscreased by " $st
-echo "fes plot from " $min " to " $max " with 51 bins and " $kt " energy units " 
-for i in `seq 1 $st $bl`; do python3 do_block_fes.py $1 1 $min $max 51 $kt $i; done
-for i in `seq 1 $st $bl`; do a=`awk '{tot+=exp(-$2/'$kt')*$3;norm+=exp(-$2/'$kt')}END{print tot/norm}' fes.$1.$i.dat`; echo $i $a; done > err.$1.blocks
+temp=2.49
+
+infile=$1	# input file, it must contain at least 1 CV in field $field and the last column is a weight
+field=$2
+NR=$3		# number of replica
+temp=$4 # temperature in energy units
+
+if [ ! $NR ]; then NR=1; fi
+if [ ! $temp ]; then temp=2.49; fi
+
+echo -e "Considering $NR replica\n"
+
+
+if [ -f "err.cv$field.blocks" ]; then mv err.cv$field.blocks err.cv$field.blocks.bck; fi
+if [ -f "errweight.cv$field.blocks" ]; then mv errweight.cv$field.blocks errweight.cv$field.blocks.bck; fi
+
+
+awk -v field=$field '{if($1!="#!") print $field, $NF}' $infile > cv1w
+maxl=`wc -l cv1w | awk '{printf("%i\n", $1)}'`
+
+max=`sort -k1 -n cv1w | tail -1 | awk '{print $1}'`
+min=`sort -k1 -n -r cv1w | tail -1 | awk '{print $1}'`
+echo "fes plot from " $min " to " $max " with 51 bins and " $temp " energy units " 
+
+
+for ((interval=20;interval>0;interval--)); do
+	i=`echo $interval | awk '{printf("%i\n", '$maxl'/($1*'$NR'))}'`
+	echo -e "\nEach of the $NR replica is divided in $interval blocks, with block-length $i"
+	python do_block_fes.py cv1w 1 $min $max 51 $temp $i; 
+	mv fes.$i.dat fes.cv$field.$i.dat;
+
+	# error
+	a=`awk '{tot+=$3}END{print tot/NR}' fes.cv$field.$i.dat`;
+       	echo $i $a >> err.cv$field.blocks
+	b=`awk -v kt=$temp '{tot+=$3*exp(-$2/kt);n+=exp(-$2/kt)}END{print tot/n}' fes.cv$field.$i.dat`; 
+	echo $i $b >> errweight.cv$field.blocks
+	
+done
+mv cv1w cv$field.dat;
+
